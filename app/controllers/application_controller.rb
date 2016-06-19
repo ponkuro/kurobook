7538435f5c2b11c662c7224eb59a5929dd766de0
Hiroyuki Kurose <pon.kurose.web@gmail.com>
2016-06-19 15:23:48 +0000

    今度こそ基本機能が完成！！

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protected
     def configure_permitted_parameters
       devise_parameter_sanitizer.for(:account_update) << :name
+      devise_parameter_sanitizer.for(:account_update) << :email
       devise_parameter_sanitizer.for(:account_update) << :image
       devise_parameter_sanitizer.for(:account_update) << :profile
     end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -5,8 +5,8 @@ class RelationshipsController < ApplicationController
     @user = User.find(params[:relationship][:receiver_id])
     current_user.follow!(@user)
     respond_to do |format|
-      format.html { redirect_to :back, notice: 'フレンド申請を送りました。' }
-      format.js {render :redraw, notice: 'フレンド申請を送りました。'}
+      format.html { redirect_to :back }
+      format.js {render :redraw }
     end
   end
 
@@ -14,8 +14,8 @@ class RelationshipsController < ApplicationController
     @user = Relationship.find(params[:id]).receiver
     current_user.unfollow!(@user)
     respond_to do |format|
-      format.html { redirect_to :back, notice: 'フレンドを解除しました。' }
-      format.js {render :redraw, notice: 'フレンドを解除しました。'}
+      format.html { redirect_to :back }
+      format.js {render :redraw }
     end
   end
 end
